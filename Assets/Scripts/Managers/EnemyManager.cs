c69adc1bf2faa7763b9a35848cb99bad484d4e54
PlzCarryMe <riccowiguna1@gmail.com>
2021-12-14 21:51:20 +0700

    Initial commit

---
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public GameObject enemy;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+
+    //[SerializeField]
+    //MonoBehaviour factory;
+    //IFactory Factory { get { return factory as IFactory; } }
+
+    void Start ()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn ()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            return;
+        }
+
+        int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+        //int spawnEnemy = Random.Range(0, 3);
+
+        //Memduplikasi enemy
+        //Factory.FactoryMethod(spawnEnemy);
+
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+
+    }
+}
